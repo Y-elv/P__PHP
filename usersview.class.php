687101fb774b651f6ac52a0cccc25c88d01c1655
Y-elv <mugishaelvis456@gmail.com>
2024-01-20 15:19:38 +0200

    adding few things

--- usersview.class.php
+++ usersview.class.php
@@ -1,5 +1,17 @@
 <?php
 
-class UsersView extends Users{
-    
+class UsersView extends Users {
+
+    public function showUser($name) {
+        $results = $this->getUser($name);
+
+        if (!empty($results)) {
+            echo "Full name: " . $results[0]['first_name'] . " " . $results[0]['second_name'] . "<br>";
+            echo "Date of Birth: " . $results[0]['age'];
+        } else {
+            echo "User not found";
+        }
+    }
+
 }
+?>
